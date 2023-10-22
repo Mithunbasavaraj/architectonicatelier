1d677e7a99f5243fad5a35bacb2a5816735df581
mithun <mithunbasavaraj057@gmail.com>
2023-10-22 19:03:10 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -220,28 +220,3 @@
     initJarallax();
   }); // End of document ready
 })(jQuery);
-new Swiper(".testimonials-slider", {
-  speed: 600,
-  loop: true,
-  autoplay: {
-    delay: 5000,
-    disableOnInteraction: false,
-  },
-  slidesPerView: "auto",
-  pagination: {
-    el: ".swiper-pagination",
-    type: "bullets",
-    clickable: true,
-  },
-  breakpoints: {
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 20,
-    },
-
-    1200: {
-      slidesPerView: 1,
-      spaceBetween: 20,
-    },
-  },
-});
